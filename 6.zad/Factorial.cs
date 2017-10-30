514bfef0cf83ce34f502de34f26e5fef48024f1f
EddieM123 <eduardo.maric12@gmail.com>
2017-10-31 00:40:31 +0100

    Add files via upload

---
+++ 6.zad/Factorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.zad
+{
+    public class Factorial
+    {
+        public static void Main(String[] args)
+        {
+            int n = 5;
+            
+            var task = FactorialDigitSumAsync(n);
+            int result = task.Result;
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+
+        public static async Task<int> FactorialDigitSumAsync(int n)
+        {
+           int x = await count(n);
+           return x;
+        }
+
+        public static async Task<int> count(int x)
+        {
+            int suma = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                suma = suma * i;
+            }
+            return suma;
+        }
+    }
+}
